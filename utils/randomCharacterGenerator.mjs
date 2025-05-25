c9c491d750ef19aa67b7bd6c2f4dfb4f3c50fef1
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-05-25 02:53:47 -0500

    utils for random character and int genenrator

---
+++ utils/randomCharacterGenerator.mjs
@@ -0,0 +1,8 @@
+import {getRandomIntInclusive} from './randomInt.mjs'
+
+export default function randomCharacter() {
+    let c = getRandomIntInclusive(32,127)
+    return String.fromCharCode(c)
+}
+
+console.log(randomCharacter())
---
+++ utils/randomInt.mjs
@@ -0,0 +1,5 @@
+export function getRandomIntInclusive(min=0, max=1) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+  }
