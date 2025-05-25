7c7d5ac6195fda3b701dd3f2939aeaae382b2552
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-05-25 05:47:04 -0500

    utils function

--- utils/randomInt.mjs
+++ utils/randomInt.mjs
@@ -1,5 +1,13 @@
 export function getRandomIntInclusive(min=0, max=1) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
   }
+
+export function getRandomNumberInclusive(min=0, max=1) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.random() * (maxFloored - minCeiled) + minCeiled;
+}
+
+console.log(getRandomIntInclusive(0,5))
