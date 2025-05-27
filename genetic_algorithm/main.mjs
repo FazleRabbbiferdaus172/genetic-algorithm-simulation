4833ece02276d495a743f2672ca494d1f82a77e6
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-05-27 13:40:43 -0500

    intial refactor to move all logics to a class of genetic algotrithm

--- genetic_algorithm/main.mjs
+++ genetic_algorithm/main.mjs
@@ -2,82 +2,135 @@ import { DNA } from "./dna.mjs"
 import FitnessCalculator from "./populationFitnessCalculator.mjs"
 import MatingPool from "./matingPool.mjs"
 
-let mutationRate = 0.01
-let populationSize = 100
-let population = []
-let pool = []
-const target = "Faris Al Mahmud"
-const DnaLength = target.length
-
-DNA.mutationRate = mutationRate
+//let mutationRate = 0.01
+//let populationSize = 100
+//let population = []
+//let pool = []
+//const target = "Faris Al Mahmud"
+//const DnaLength = target.length
+//DNA.mutationRate = mutationRate
 // console.log(DNA.mutationRate)
 
-const fitnessCalculator = new FitnessCalculator(target)
 
-function setup() {
-    for (let i = 0; i<populationSize; i++) {
-        let creature = new DNA(DnaLength, fitnessCalculator)
-        creature.fillGenes()
-        population.push(creature)
+//function setup() {
+//    for (let i = 0; i<populationSize; i++) {
+//        let creature = new DNA(DnaLength, fitnessCalculator)
+//        creature.fillGenes()
+//        population.push(creature)
+//    }
+//}
+
+//function evaluatePulationFitness() {
+//    for (const p of population) {
+//        p.calculateFitness()
+//    }
+//}
+
+//function fillWithNewPopulation(pool) {
+//    for (let i = 0; i < populationSize; i++) {
+//        let parent1 = pool.pickFromPool()
+//        let parent2 = pool.pickFromPool()
+//        population[i] = parent1.giveBirth(parent2)
+//    }
+//}
+
+// function findTheBestFit () {
+//    let result = population[0]
+//    let currentFitness = result.fitness
+
+//    for (const p of population) {
+//        if (currentFitness < p.fitness) {
+//            currentFitness = p.fitness
+//            result = p
+//        }
+//    }
+//    return result
+//}
+
+
+
+//while (true) {
+//    let state = runStep(population, pool)
+//    population = state.population
+//    pool = state.pool
+//    console.log(state.bestFit.gense)
+//    if (state.end) {
+//        break
+//    }
+//}
+
+
+class GeneticAlgorithm {
+    constructor(target = "Farish Al Mahmud", populationSize = 100, mutationRate = 0.01, ) {
+        this.target = target
+        this.DnaLength = target.length
+        this.fitnessCalculator = FitnessCalculator(target)
+        this.populationSize = populationSize
+        this.mutationRate = mutationRate
+        this.population = []
+        this.pool = []
+        this.is_setup = false
+        this.end = false
+        this.itteration = 0
+        this.bestFit = undefined
     }
-}
 
-function evaluatePulationFitness() {
-    for (const p of population) {
-        p.calculateFitness()
+    setup() {
+        DNA.mutationRate = this.mutationRate
+        for (let i = 0; i< this.populationSize; i++) {
+            let creature = new DNA(this.DnaLength, this.fitnessCalculator)
+            creature.fillGenes()
+            this.population.push(creature)
+        }
+        this.is_setup = true        
     }
-}
-
-function fillWithNewPopulation(pool) {
-    for (let i = 0; i < populationSize; i++) {
-        let parent1 = pool.pickFromPool()
-        let parent2 = pool.pickFromPool()
-        population[i] = parent1.giveBirth(parent2)
-    }
-}
-
-function findTheBestFit () {
-    let result = population[0]
-    let currentFitness = result.fitness
-
-    for (const p of population) {
-        if (currentFitness < p.fitness) {
-            currentFitness = p.fitness
-            result = p
+    
+    evaluatePulationFitness() {
+        for (const p of this.population) {
+            p.calculateFitness()
         }
     }
-    return result
+
+    fillWithNewPopulation(pool) {
+        let parent1;
+        let parent2;
+        for (let i = 0; i < populationSize; i++) {
+            parent1 = pool.pickFromPool()
+            parent2 = pool.pickFromPool()
+            this.population[i] = parent1.giveBirth(parent2)
+        }
+    }
+
+    findTheBestFit () {
+        let result = this.bestFit ?? population[0]
+        let currentFitness = result.fitness
+
+        for (const p of this.population) {
+            if (currentFitness < p.fitness) {
+                currentFitness = p.fitness
+                result = p
+            }
+        }
+        return result
+    }
+
+    runStep() {
+        this.itteration += 1
+        if (this.population.length === 0) {
+            setup()
+        }
+        evaluatePulationFitness()
+        this.bestFit = findTheBestFit()
+        if (this.bestFit.fitness == 1) {
+            this.end = true
+            // return {end: true, bestFit: bestFit, population: this.population, pool: this.pool}
+        }
+        else {
+            this.pool = new MatingPool(this.population)
+            fillWithNewPopulation(this.pool)
+        }
+        this.population = population
+        this.pool = pool       
+    }
+
 }
-
-
-function runStep(population=[], pool=[]) {
-    if (population.length === 0) {
-        setup()
-    }
-    evaluatePulationFitness()
-    let bestFit = findTheBestFit()
-    if (bestFit.fitness == 1) {
-        return {end: true, bestFit: bestFit, population: population, pool: pool}
-    }
-    else {
-        pool = new MatingPool(population)
-        fillWithNewPopulation(pool)
-        return {end: false, bestFit: bestFit, population: population, pool: pool}
-    }
-
-}
-
-while (true) {
-    let state = runStep(population, pool)
-    population = state.population
-    pool = state.pool
-    console.log(state.bestFit.gense)
-    if (state.end) {
-        break
-    }
-}
-
-
-// const GeneticAlgorithm = {
-
-// }
